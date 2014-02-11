2bd68c2793e098ed7e41a29ac126836842a526bc
Marvin Steadfast <xsteadfastx@gmail.com>
2014-02-11 17:47:44 +0100

    added first dirty version

---
+++ README.md
@@ -0,0 +1,10 @@
+art_millionair
+==============
+
+1. `virtualenv -p /usr/bin/python3 art_millionair`
+2. `cd art_millionair`
+3. `. bin/activate`
+4. `git clone https://github.com/xsteadfastx/art_millionair.git`
+5. `cd art_millionair`
+6. `pip install -r requirements.txt`
+7. `python art_millionair.py example`
---
+++ art_millionair.py
@@ -0,0 +1,57 @@
+import random
+import glob
+import sys
+import os.path
+from flask import Flask, render_template, session
+from flask_bootstrap import Bootstrap
+
+
+app = Flask(__name__, static_folder=sys.argv[1])
+Bootstrap(app)
+
+directory = sys.argv[1]
+
+def get_questions():
+    questions = []
+    for file in sorted(glob.glob(directory+'/*.txt')):
+        questions.append(open(file, 'r').read().splitlines())
+
+    return questions
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/<int:question_number>/question')
+def question_number(question_number):
+    questions_extract = get_questions()[question_number]
+    session['question'] = questions_extract[0]
+    session['right_answer'] = questions_extract[1]
+    answers = []
+    for i in questions_extract[-4:]:
+        answers.append(i)
+
+    random.shuffle(answers)
+    session['answers'] = answers
+
+    image_file = '%s/%s.jpg' % (directory, question_number)
+    if os.path.isfile(image_file):
+        image = True
+    else:
+        image = False
+
+    return render_template('question.html', question_number=question_number, 
+                           image=image, image_file=image_file)
+
+
+@app.route('/<int:question_number>/result/<guess>')
+def result(question_number, guess):
+    return render_template('result.html', question_number=question_number, 
+                           guess=guess)
+
+
+if __name__ == "__main__":
+    app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+    app.run()
