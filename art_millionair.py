3f593f0be372fa81f9b24dfd2935052310f89d0e
Marvin Steadfast <xsteadfastx@gmail.com>
2014-02-12 11:31:42 +0100

    new create_answer_list function and implemented 50/50 joker

--- art_millionair.py
+++ art_millionair.py
@@ -6,10 +6,11 @@ from flask import Flask, render_template, session, redirect
 from flask_bootstrap import Bootstrap
 
 
-app = Flask(__name__, static_folder=sys.argv[1])
+directory = sys.argv[1]
+
+app = Flask(__name__, static_folder=directory)
 Bootstrap(app)
 
-directory = sys.argv[1]
 
 def get_questions():
     questions = []
@@ -19,22 +20,19 @@ def get_questions():
     return questions
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/<int:question_number>/question')
-def question_number(question_number):
-    questions_extract = get_questions()[question_number]
+def create_answer_list(question_number):
+    questions_extract = session['question_extract'][question_number]
     session['question'] = questions_extract[0]
     session['right_answer'] = questions_extract[1]
     answers = []
-    for i in questions_extract[-4:]:
-        answers.append(i)
+    answers.append(session['right_answer'])
+    if '5050' in session and session['5050'] == question_number:
+        answers.append(random.choice(questions_extract[-3:]))
+    else:
+        for i in questions_extract[-3:]:
+            answers.append(i)
 
     random.shuffle(answers)
-    session['answers'] = answers
 
     session['image_file'] = '%s/%s.jpg' % (directory, question_number)
     if os.path.isfile(session['image_file']):
@@ -42,12 +40,26 @@ def question_number(question_number):
     else:
         session['image'] = False
 
-    return render_template('question.html', question_number=question_number) 
+    return answers
+
+
+@app.route('/')
+def index():
+    session['question_extract'] = get_questions()
+
+    return render_template('index.html')
+
+
+@app.route('/<int:question_number>/question')
+def question_number(question_number):
+    session['answers'] = create_answer_list(question_number)
+
+    return render_template('question.html', question_number=question_number)
 
 
 @app.route('/<int:question_number>/result/<guess>')
 def result(question_number, guess):
-    return render_template('result.html', question_number=question_number, 
+    return render_template('result.html', question_number=question_number,
                            guess=guess)
 
 
@@ -61,7 +73,15 @@ def joker_phone_audience(question_number, phone_or_audience):
         pass
 
     url = '/%s/question' % (question_number)
-    return redirect(url) 
+    return redirect(url)
+
+
+@app.route('/<int:question_number>/joker/5050')
+def joker_5050(question_number):
+    session['5050'] = question_number
+    session['answers'] = create_answer_list(question_number)
+
+    return render_template('question.html', question_number=question_number)
 
 
 if __name__ == "__main__":
